8e946480770ba7e74d1600bd9cc076c901d1e17d
xkilian <fmikus@acktomic.com>
2016-02-23 16:30:49 -0500

    Add notes

--- plugins/genConfig/IbmIMM_x3550.pm
+++ plugins/genConfig/IbmIMM_x3550.pm
@@ -175,7 +175,7 @@ sub custom_targets {
     $file->writetarget("service {", '',
                'host_name'           => $opts->{devicename},
                'service_description' => "health_statistic_" . $id,
-              #'notes'               => $ldesc,
+               'notes'               => "IBM IMM Health Stats",
                'display_name'        => "Health statistic " . $id,
                '_inst'               => $id,
                '_dstemplate'         => "ibm-system-health-stat",
